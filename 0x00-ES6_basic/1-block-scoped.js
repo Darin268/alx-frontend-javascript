f52a81c08990269f6ae6243a46e06a00be72d14c
“Dino” <daringamaleldin@hotmail.com>
2024-06-24 15:33:23 -0700

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,9 @@
-// 1-block-scoped.js
-
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Use let instead of var for block-scoped variable
-  const task2 = true; // Use const for constants that shouldn't change
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true; // Reassign the block-scoped variable
-    // task2 = false; // Uncommenting this line will cause an error since task2 is declared as const
+    return [task, task2];
   }
-
   return [task, task2];
 }
